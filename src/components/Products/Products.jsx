2cf17f21f165bf1e8d3393a76cc703e1b0addf9b
Mark Ekperi <mark.ekperi@gmail.com>
2020-12-18 22:20:55 +0100

    created products and props

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,13 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products =[
-    { id: 1, name: 'Shoe', description: 'Running Shoes.', price: '$20', image: 'https://cdn.pixabay.com/photo/2014/06/18/18/42/running-shoe-371625_960_720.jpg'},
-    { id: 2, name: 'Lenovo', description: 'Lenovo Thinkpad.', price: '$500', image: 'https://cdn.pixabay.com/photo/2020/03/01/10/26/laptop-4892244_960_720.jpg'},
-
-]
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
 
     return (
@@ -19,7 +13,7 @@ const Products = () => {
             <Grid container justify='center' spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
 
