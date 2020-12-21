aed680621f3d0c905c021ba6f62e3a3e41084f19
doubi3 <mark.ekperi@gmail.com>
2020-12-21 21:59:15 +0100

    added .env to .gitignore

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
+
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    );
+
+    const Form = () => activeStep === 0 
+        ? <AddressForm />
+        : <PaymentForm />
 
     return (
         <>
@@ -19,13 +31,14 @@ const Checkout = () => {
                             <Step key={step}>
                                 <StepLabel>{step}</StepLabel>
                             </Step>
-                        ))}
+                        ))};
                     </Stepper>
+                    {activeStep === steps.length ? <Confirmation /> : <Form />}
                </Paper>
            </main>
         </>
     )
-}
+};
 
 export default Checkout;
 
